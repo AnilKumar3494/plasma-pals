c2ae76e9fbe559dfd4e2248fcec3e94fd5297ed7
Nik Slivinski <nikwsli@gmail.com>
2025-03-01 17:51:33 -0500

    Current state of DTL

---
+++ plasma-pals-backend/dtkSK.py
@@ -0,0 +1,31 @@
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from sklearn import metrics
+import graphviz
+
+#df = read_csv('survey-test-data.csv', header=0)
+df = read_csv('~/Documents/GitHub/plasma-pals/plasma-pals-backend/survey-test-data.csv', header=0)
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=1)
+
+clf = DecisionTreeClassifier(criterion="entropy")
+
+clf = clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+dot_data = export_graphviz(clf, out_file=None, 
+                           feature_names=df.columns[:-1],  
+                           class_names=['No', 'Yes'],  
+                           filled=True, rounded=True,  
+                           special_characters=True)  
+
+graph = graphviz.Source(dot_data)  
+graph.render("iris_decision_tree")
+graph.view() 
